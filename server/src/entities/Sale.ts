099070fc5cc6aa31e64c671393007b3d4c2508fc
AliAlAbbassi <alialabbassi2001@gmail.com>
2021-07-31 20:40:00 +0300

    sale entity

---
+++ server/src/entities/Sale.ts
@@ -0,0 +1,41 @@
+import { Field, ObjectType } from 'type-graphql'
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+
+@ObjectType()
+@Entity()
+export class Sale extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn()
+  saleId!: number
+
+  @Field(() => Number)
+  @Column({ type: 'decimal' })
+  salePrice!: number
+
+  @Field(() => Number)
+  @Column({ type: 'decimal' })
+  bidId!: number
+
+  @Field(() => Number)
+  @Column({ type: 'decimal' })
+  askId!: number
+
+  @Field(() => Number)
+  @Column({ type: 'decimal' })
+  keebId!: Number
+
+  @Field(() => String)
+  @CreateDateColumn()
+  createdAt: Date
+
+  @Field(() => String)
+  @UpdateDateColumn()
+  updatedAt: Date
+}
