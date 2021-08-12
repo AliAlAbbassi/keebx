6467c14ed8d142db8f692ec7bb23e361131d5e6f
AliAlAbbassi <alialabbassi2001@gmail.com>
2021-08-12 21:46:32 +0300

    apollo provider on layout

--- web/src/layouts/Layout.tsx
+++ web/src/layouts/Layout.tsx
@@ -1,52 +1,64 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import React from 'react'
 import styled, { createGlobalStyle } from 'styled-components';
+import { CLIENT } from '../../env';
 import NavBar from '../components/NavBar'
 
 interface LayoutProps {
   layoutType: 'Default' | 'Keyboard'
 }
 
+const client = new ApolloClient({
+  uri: CLIENT,
+  cache: new InMemoryCache(),
+  credentials: 'include'
+})
+
 const Layout: React.FC<LayoutProps> = ({ children, layoutType = 'Default' }) => {
   if (layoutType === 'Keyboard') {
     return (
-      <LayoutContainerKeyboard>
-        <GlobalStyle />
-        <NavBar withSpaceBar={true} />
-        <main>{children}</main>
-      </LayoutContainerKeyboard>
+      <ApolloProvider client={client}>
+        <LayoutContainerKeyboard>
+          <GlobalStyle />
+          <NavBar withSpaceBar={true} />
+          <main>{children}</main>
+        </LayoutContainerKeyboard>
+      </ApolloProvider>
     )
   }
 
   return (
-    <LayoutContainer>
-      <GlobalStyle />
-      <NavBar withSpaceBar={false} />
-      <main>{children}</main>
-    </LayoutContainer>
+    <ApolloProvider client={client}>
+      <LayoutContainer>
+        <GlobalStyle />
+        <NavBar withSpaceBar={false} />
+        <main>{children}</main>
+      </LayoutContainer>
+    </ApolloProvider>
   )
 }
 
 const GlobalStyle = createGlobalStyle`
-        html{
-            margin:0;
-            padding: 0;
-        }
+      html{
+        margin:0;
+      padding: 0;
+      }
 
-        body{
-            min-height:100vh;
-            margin: 0;
-        }
-`
+      body{
+        min-height: 100vh;
+      margin: 0;
+      }
+      `
 const LayoutContainer = styled.div`
-  background: rgb(0,139,246);
-  background: linear-gradient(0deg, rgba(0,139,246,1) 0%, rgba(0,245,255,1) 50%, rgba(0,255,136,1) 100%);
-  width: 100vw;
-  margin: 0px;
-`
+      background: rgb(0,139,246);
+      background: linear-gradient(0deg, rgba(0,139,246,1) 0%, rgba(0,245,255,1) 50%, rgba(0,255,136,1) 100%);
+      width: 100vw;
+      margin: 0px;
+      `
 
 const LayoutContainerKeyboard = styled.div`
-  width: 100vw;
-  margin: 0px;
-`
+      width: 100vw;
+      margin: 0px;
+      `
 
 export default Layout
--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,14 +1,7 @@
 import type { NextComponentType } from 'next';
 import { AppContext, AppInitialProps, AppLayoutProps } from 'next/app';
 import React, { ReactNode } from 'react';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
-import { CLIENT } from '../../env';
 
-const client = new ApolloClient({
-    uri: CLIENT,
-    cache: new InMemoryCache(),
-    credentials: 'include'
-})
 
 const MyApp: NextComponentType<AppContext, AppInitialProps, AppLayoutProps> = ({
     Component,
@@ -16,9 +9,7 @@ const MyApp: NextComponentType<AppContext, AppInitialProps, AppLayoutProps> = ({
 }: AppLayoutProps) => {
     const getLayout = Component.getLayout || ((page: ReactNode) => page);
     return getLayout(
-        <ApolloProvider client={client}>
-            <Component {...pageProps} />
-        </ApolloProvider>
+        <Component {...pageProps} />
     )
 }
 
